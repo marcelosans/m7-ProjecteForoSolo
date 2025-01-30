41eb1eb8b77ff3ffe1914144bcf5acd4914bb05d
Marcel Lejk <marcel.lejk@educem.net>
2025-01-30 14:50:31 +0100

    acabar el css

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ if (!isset($_SESSION['email'])) {
     header('Location: login.php'); 
     exit;
 }
-echo 'Bienvenido seas: ';
-echo $_SESSION['email'];
+echo '<h1>Bienvenido seas: </h1>';
+echo '<h2>'.  $_SESSION['email'] . '</h2>';
 
 
 ?>
@@ -15,10 +15,15 @@ echo $_SESSION['email'];
         <meta charset="utf-8">
         <title>Inicio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
     </head>
     <body>
     <form action="cerrarSesion.php" method="POST">
+    <img src="./img/pescando.gif" alt="hombre-pescando">
+    <br>
     <button type="submit" value="cerrar-session">Cerrar Sessión</button>
 </form>
     </body>
