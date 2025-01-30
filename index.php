d4eeeef02c041594ec3f346932e6d89aae0c9816
Alexcoti284 <118183101+Alexcoti284@users.noreply.github.com>
2025-01-30 15:16:16 +0100

    Index modificar

--- index.php
+++ index.php
@@ -4,27 +4,25 @@ if (!isset($_SESSION['email'])) {
     header('Location: login.php'); 
     exit;
 }
-echo '<h1>Bienvenido seas: </h1>';
-echo '<h2>'.  $_SESSION['email'] . '</h2>';
-
-
 ?>
 <!DOCTYPE html>
 <html lang="ca">
-    <head>
-        <meta charset="utf-8">
-        <title>Inicio</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./css/style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
-    </head>
-    <body>
-    <form action="cerrarSesion.php" method="POST">
-    <img src="./img/pescando.gif" alt="hombre-pescando">
-    <br>
-    <button type="submit" value="cerrar-session">Cerrar Sessión</button>
-</form>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>ForoSolo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="login-container">
+        <h1>Bienvenido seas:</h1>
+        <h2><?php echo $_SESSION['email']; ?></h2>
+        <form action="cerrarSesion.php" method="POST">
+            <button class="btn" type="submit" value="cerrar-session">Cerrar Sesión</button>
+        </form>
+    </div>
+</body>
 </html>
