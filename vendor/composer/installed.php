29ec6744996d83804128f05a57ddbaf855e9b371
Alexcoti284 <118183101+Alexcoti284@users.noreply.github.com>
2025-02-13 15:04:12 +0100

    Enviar correo todo

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a570400a9f85cd677cc07cf431ff93cabe2d48bb',
+        'reference' => 'c5c750c4e871b4cdffa09dabf17995634e41fe1d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a570400a9f85cd677cc07cf431ff93cabe2d48bb',
+            'reference' => 'c5c750c4e871b4cdffa09dabf17995634e41fe1d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
